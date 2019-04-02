77a37f70bab2fbdc3dff29a47baa8de78b0ae935
PiotrTomaszewski <piotr.tomaszewski@codecool.com>
2019-04-02 18:52:11 +0200

    add home page to our project

--- src/main/java/com/spring/config/DemoAppConfig.java
+++ src/main/java/com/spring/config/DemoAppConfig.java
@@ -1,11 +1,21 @@
 package com.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.spring")
-public class DemoAppConfig { }
+public class DemoAppConfig {
+    @Bean
+    public InternalResourceViewResolver getViewResolver(){
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/WEB-INF/view/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return internalResourceViewResolver;
+    }
+}
 
