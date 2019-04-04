5489ba311c3e30f39186eeef7527c7e9c6aac179
PiotrTomaszewski <piotr.tomaszewski@codecool.com>
2019-04-04 11:47:49 +0200

    implemented all CRUD operations on data

--- src/main/java/com/spring/controller/CustomerRestController.java
+++ src/main/java/com/spring/controller/CustomerRestController.java
@@ -2,10 +2,7 @@ package com.spring.controller;
 import com.spring.entity.Customer;
 import com.spring.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,5 +28,32 @@ public class CustomerRestController {
             throw new CustomerNotFoundException("customer id not found - "+customerId);
         return customer;
     }
+
+    //add mapping for POST /customers - add new customer
+    @PostMapping("/customers")
+    public Customer addCustomer(@RequestBody Customer customer){
+        customer.setId(0);
+        customerService.saveCustomer(customer);
+        return customer;
+    }
+
+    //add mapping for PUT /customers - update the customer
+    @PutMapping("/customers")
+    public Customer updateCustomer(@RequestBody Customer customer){
+        customerService.saveCustomer(customer);
+        return customer;
+    }
+
+    //add mapping .=fur DELETE /customers - delete customer
+    @DeleteMapping("/customers/{customerId}")
+    public String deleteCustomer(@PathVariable int customerId){
+        Customer customer = customerService.getCustomer(customerId);
+        if(customer == null)
+            throw new CustomerNotFoundException("Customer ID not found - "+customerId);
+
+        customerService.deleteCustomer(customerId);
+        return "deleted customer id - "+customerId;
+    }
 }
+
 
